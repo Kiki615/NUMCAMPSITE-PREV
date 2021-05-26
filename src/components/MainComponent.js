983bcacd30163b47d5aab82b21155ac704e0cd52
Kianush Dixon <kianushdixon@gmail.com>
2021-05-25 22:28:15 -0500

    Begin Forms Exercise...

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
@@ -34,6 +35,17 @@ class Main extends Component {
           );
       };
 
+
+      const AboutUsPage = () => {
+        return (
+            <About 
+            partners={this.state.partners}
+            />
+        );
+    };
+
+
+
       const CampsiteWithId = ({match}) => {
         return (
            <CampsiteInfo 
@@ -51,6 +63,7 @@ class Main extends Component {
                 <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites}/>} />
                 <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                 <Route exact path='/contactus' component={Contact} />
+                <Route exact path='/aboutus' component={AboutUsPage} />
                 <Redirect to='/home' />
                 <Directory campsites={this.state.campsites} />
               </Switch>
